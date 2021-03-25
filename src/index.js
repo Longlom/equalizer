363857ec2661869e3c1729857169f899f0604dd9
trunqh <hqtxxx@gmail.com>
2021-03-25 17:21:25 +0300

    feat: update index file

--- src/index.js
+++ src/index.js
@@ -1,46 +1,60 @@
+import { listBand } from "constants/listBand";
+import { AudioSchema } from "context/AudioSchema";
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
+import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import { AudioContext } from "context/audioContext";
-import audioSource from "sg.mp3";
-import { feedBackward, feedForward } from "constants/coefficients";
+
 const WebContextAudio = window.AudioContext || window.webkitAudioContext;
 const audioContext = new WebContextAudio();
-
-const audioElement = new Audio(audioSource);
-const track = audioContext.createMediaElementSource(audioElement);
-const mergeNode = new ChannelMergerNode(audioContext, { numberOfInputs: 8 });
-
-const nodes = [];
-for (let i = 0; i < 8; i++) {
-  const filterNode = new IIRFilterNode(audioContext, {
-    feedforward: feedForward[i],
-    feedback: feedBackward[i],
-  });
-  const gainNode = new GainNode(audioContext);
-  track.connect(filterNode).connect(gainNode).connect(mergeNode);
-  nodes.push({
-    filterNode,
-    gainNode,
-  });
-}
-console.log(nodes);
 const gainNode = new GainNode(audioContext);
-mergeNode.connect(gainNode).connect(audioContext.destination);
+
+const filterNodes = [];
+for (let i = 0; i < 8; i++) {
+  const filterNode = new BiquadFilterNode(audioContext, {
+    type: "peaking",
+  });
+  filterNode.frequency.value = (listBand[i].min + listBand[i].max) / 2;
+  filterNodes.push(filterNode);
+  if (i > 0) filterNodes[i - 1].connect(filterNode);
+  if (i === 7) {
+    filterNode.connect(gainNode);
+  }
+}
+
+const delayNode = new DelayNode(audioContext, { delayTime: 0.6 });
+const gainDelayNode = new GainNode(audioContext, { gain: 0.4 });
+gainDelayNode.connect(delayNode);
+
+const vibratoNode = new GainNode(audioContext, { gain: 0 });
+const shaperNode = new WaveShaperNode(audioContext, {
+  curve: new Float32Array([0, 1]),
+});
+
+gainNode.connect(audioContext.destination);
+gainNode.connect(vibratoNode);
+gainNode.connect(gainDelayNode);
+audioContext.suspend();
+
 ReactDOM.render(
-  <AudioContext.Provider
-    value={{ audioContext, audioElement, nodes, gainNode }}
-  >
-    <React.StrictMode>
+  <React.StrictMode>
+    <AudioSchema.Provider
+      value={{
+        audioContext,
+        listNodes: {
+          filterNodes,
+          gainNode,
+          delayNode,
+          vibratoNode,
+          shaperNode,
+        },
+      }}
+    >
       <App />
-    </React.StrictMode>
-  </AudioContext.Provider>,
+    </AudioSchema.Provider>
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
