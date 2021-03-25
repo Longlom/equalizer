2c0002b36fe6835fec7da330f8baabb1b6e686dc
trunqh <hqtxxx@gmail.com>
2021-03-25 17:02:18 +0300

    feat: edit list panel

--- src/components/ListPanel.js
+++ src/components/ListPanel.js
@@ -1,21 +1,18 @@
 import { makeStyles, Paper } from "@material-ui/core/";
-import React from "react";
 import { listBand } from "constants/listBand";
+import { useAudioSchema } from "context/AudioSchema";
+import React from "react";
 import Panel from "./Panel";
-import { useAudioContext } from "context/audioContext";
 const ListPanel = () => {
   const classes = useStyles();
-  const { nodes } = useAudioContext();
+  const { listNodes } = useAudioSchema();
+  const { filterNodes } = listNodes;
   return (
-    <div className={classes.listPanel}>
+    <Paper className={classes.listPanel}>
       {listBand.map((band, index) => (
-        <Panel
-          key={index}
-          frequency={band.frequency}
-          gainNode={nodes[index].gainNode}
-        />
+        <Panel key={index} frequency={band.max} filter={filterNodes[index]} />
       ))}
-    </div>
+    </Paper>
   );
 };
 const useStyles = makeStyles({
